139f0003f21ef88d0528865630696b5a3df545d2
Tobias Pott <mailme@tobiaspott.de>
2023-10-04 23:55:13 +0200

    Fix light transformation

--- MonoGame.LibDeferred/Lighting/PointLight.cs
+++ MonoGame.LibDeferred/Lighting/PointLight.cs
@@ -75,17 +75,6 @@ namespace DeferredEngine.Entities
             Name = GetType().Name + " " + Id;
         }
 
-        public override int Id { get; set; }
-        public override Matrix RotationMatrix { get; set; }
-
-        public sealed override bool IsEnabled
-        {
-            get;
-            set;
-        }
-
-        public override string Name { get; set; }
-
         public Color Color
         {
             get { return _color; }
@@ -121,6 +110,17 @@ namespace DeferredEngine.Entities
                 HasChanged = true;
             }
         }
+
+        public override int Id { get; set; }
+        public override Matrix RotationMatrix { get; set; }
+
+        public sealed override bool IsEnabled
+        {
+            get;
+            set;
+        }
+
+        public override string Name { get; set; }
 
         protected PointLight()
         {
