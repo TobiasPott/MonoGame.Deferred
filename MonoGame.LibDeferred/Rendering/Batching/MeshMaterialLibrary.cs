7cf118f85e589c52c95107f0737965206581ad96
Tobias Pott <mailme@tobiaspott.de>
2023-10-06 22:07:53 +0200

    Fixed transform update in mesh material batching library

--- MonoGame.LibDeferred/Rendering/Batching/MeshMaterialLibrary.cs
+++ MonoGame.LibDeferred/Rendering/Batching/MeshMaterialLibrary.cs
@@ -223,6 +223,23 @@ namespace DeferredEngine.Renderer.Helper
 
             if (!RenderingSettings.g_cpuculling) return false;
 
+            for (int index1 = 0; index1 < entities.Count; index1++)
+            {
+                ModelEntity entity = entities[index1];
+
+                if (!hasCameraChanged && !entity.WorldTransform.HasChanged)
+                {
+                    continue;
+                }
+
+                if (entity.WorldTransform.HasChanged)
+                {
+                    entity.WorldTransform.World = entity.World;
+                    entity.WorldTransform.InverseWorld = entity.InverseWorld;
+                    entity.WorldTransform.Scale = entity.Scale;
+                }
+            }
+
             bool hasAnythingChanged = false;
             //Ok we applied the transformation to all the entities, now update the submesh boundingboxes!
             // Parallel.For(0, Index, index1 =>
