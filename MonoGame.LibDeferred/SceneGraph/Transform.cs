7e90e25ea5b356f6b10db62ff2aea781f2c2ad37
Tobias Pott <mailme@tobiaspott.de>
2023-10-06 21:38:46 +0200

    Fixed wrong inverse world transform return value

--- MonoGame.LibDeferred/SceneGraph/Transform.cs
+++ MonoGame.LibDeferred/SceneGraph/Transform.cs
@@ -66,7 +66,7 @@ namespace DeferredEngine.Entities
                     _inverseWorld = Matrix.Invert(_world);
                     _worldHasChanged = false;
                 }
-                return _world;
+                return _inverseWorld;
             }
         }
 
