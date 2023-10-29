bd1dc52958251a9672e2cad12541e01ea915a422
Tobias Pott <mailme@tobiaspott.de>
2023-10-29 20:39:23 +0100

    Fixed UI not changing TAA booleans

--- MonoGame.Deferred/Logic/GUILogic.cs
+++ MonoGame.Deferred/Logic/GUILogic.cs
@@ -215,25 +215,25 @@ namespace DeferredEngine.Logic
 
             postprocessingList.AddElement(new GUITextBlockToggle(defaultStyle, "Post Processing")
             {
-                ToggleField = typeof(RenderingSettings).GetField(nameof(RenderingSettings.g_PostProcessing)),
+                ToggleProperty = typeof(RenderingSettings).GetProperty(nameof(RenderingSettings.g_PostProcessing)),
                 Toggle = RenderingSettings.g_PostProcessing
             });
 
             postprocessingList.AddElement(new GUITextBlockToggle(defaultStyle, "Color Grading")
             {
-                ToggleField = typeof(RenderingSettings).GetField(nameof(RenderingSettings.g_ColorGrading)),
+                ToggleProperty = typeof(RenderingSettings).GetProperty(nameof(RenderingSettings.g_ColorGrading)),
                 Toggle = RenderingSettings.g_ColorGrading
             });
 
             postprocessingList.AddElement(new GUITextBlockToggle(defaultStyle, "Temporal AA")
             {
-                ToggleField = typeof(TemporalAAFx).GetField(nameof(TemporalAAFx.g_Enabled)),
+                ToggleProperty = typeof(TemporalAAFx).GetProperty(nameof(TemporalAAFx.g_Enabled)),
                 Toggle = TemporalAAFx.g_Enabled
             });
 
             postprocessingList.AddElement(new GUITextBlockToggle(defaultStyle, "Tonemap TAA")
             {
-                ToggleField = typeof(TemporalAAFx).GetField(nameof(TemporalAAFx.g_UseTonemapping)),
+                ToggleProperty = typeof(TemporalAAFx).GetProperty(nameof(TemporalAAFx.g_UseTonemapping)),
                 Toggle = TemporalAAFx.g_UseTonemapping
             });
 
--- MonoGame.LibDeferred/Settings/RenderingSettings.PostProcessing.cs
+++ MonoGame.LibDeferred/Settings/RenderingSettings.PostProcessing.cs
@@ -4,9 +4,9 @@
     public static partial class RenderingSettings
     {
 
-        public static bool g_PostProcessing = true;
+        public static bool g_PostProcessing { get; set; } = true;
 
-        public static bool g_ColorGrading = true;
+        public static bool g_ColorGrading { get; set; } = true;
 
     }
 }
