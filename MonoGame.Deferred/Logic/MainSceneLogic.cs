77aead69aa2aa16dc8dad885f9be34e6a012e227
Tobias Pott <mailme@tobiaspott.de>
2023-10-31 20:26:12 +0100

    Code organisationn

--- MonoGame.Deferred/Logic/MainSceneLogic.cs
+++ MonoGame.Deferred/Logic/MainSceneLogic.cs
@@ -4,7 +4,6 @@ using DeferredEngine.Pipeline.Lighting;
 using DeferredEngine.Recources;
 using DeferredEngine.Rendering;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
