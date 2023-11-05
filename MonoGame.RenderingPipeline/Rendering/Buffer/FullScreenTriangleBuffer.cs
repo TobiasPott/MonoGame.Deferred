3cef31604525edc3df5d1c0b1557acef5e215172
Tobias Pott <mailme@tobiaspott.de>
2023-11-05 15:00:53 +0100

    Changed fullscreen triangle to use index buffer (don't understand the position range but asume it's sth with the VertexShaderFunction code)

--- MonoGame.RenderingPipeline/Rendering/Buffer/FullScreenTriangleBuffer.cs
+++ MonoGame.RenderingPipeline/Rendering/Buffer/FullScreenTriangleBuffer.cs
@@ -3,6 +3,24 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace DeferredEngine.Rendering
 {
+    public struct FullScreenQuadVertex
+    {
+        // Stores the starting position of the particle.
+        public Vector2 Position;
+
+        public static readonly VertexDeclaration VertexDeclaration = new VertexDeclaration
+        (
+            new VertexElement(0, VertexElementFormat.Vector2, VertexElementUsage.Position, 0)
+        );
+
+        public FullScreenQuadVertex(Vector2 position)
+        {
+            Position = position;
+        }
+
+        public const int SizeInBytes = 8;
+    }
+
     public class FullscreenTriangleBuffer
     {
         #region Singleton & Static Load/Unload
@@ -15,42 +33,37 @@ namespace DeferredEngine.Rendering
 
         #endregion
 
+
+        private static VertexPositionTexture[] Vertices = new[] {
+            new VertexPositionTexture(new Vector3(-1, -1, 0), new Vector2(0, 0)),
+            new VertexPositionTexture(new Vector3(-1, 3, 0), new Vector2(0, 1)),
+            new VertexPositionTexture(new Vector3(3, -1, 0), new Vector2(1, 0)),
+            new VertexPositionTexture(new Vector3(3, 3, 0), new Vector2(1, 1))
+        };
+        private static ushort[] Indices = new ushort[] { 0, 1, 2, 2, 1, 3 };
+
+        public static readonly VertexDeclaration VertexDeclaration = new VertexDeclaration
+        (
+            new VertexElement(0, VertexElementFormat.Vector3, VertexElementUsage.Position, 0),
+            new VertexElement(12, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0)
+        );
+
         private readonly VertexBuffer _vertexBuffer;
-
-        public struct FullScreenQuadVertex
-        {
-            // Stores the starting position of the particle.
-            public Vector2 Position;
-
-            public static readonly VertexDeclaration VertexDeclaration = new VertexDeclaration
-            (
-                new VertexElement(0, VertexElementFormat.Vector2, VertexElementUsage.Position, 0)
-            );
-
-            public FullScreenQuadVertex(Vector2 position)
-            {
-                Position = position;
-            }
-
-            public const int SizeInBytes = 8;
-        }
+        private readonly IndexBuffer _indexBuffer;
 
         public FullscreenTriangleBuffer(GraphicsDevice graphics)
         {
-            FullScreenQuadVertex[] vertices = new FullScreenQuadVertex[3];
-            vertices[0] = new FullScreenQuadVertex(new Vector2(-1, -1));
-            vertices[1] = new FullScreenQuadVertex(new Vector2(-1, 3));
-            vertices[2] = new FullScreenQuadVertex(new Vector2(3, -1));
-
-            _vertexBuffer = new VertexBuffer(graphics, FullScreenQuadVertex.VertexDeclaration, 3, BufferUsage.WriteOnly);
-            _vertexBuffer.SetData(vertices);
+            _vertexBuffer = new VertexBuffer(graphics, VertexDeclaration, Vertices.Length, BufferUsage.WriteOnly);
+            _vertexBuffer.SetData(Vertices);
+            _indexBuffer = new IndexBuffer(graphics, IndexElementSize.SixteenBits, Indices.Length, BufferUsage.WriteOnly);
+            _indexBuffer.SetData(Indices);
         }
 
         public void Draw(GraphicsDevice graphics)
         {
             graphics.SetVertexBuffer(_vertexBuffer);
-            graphics.Indices = null;
-            graphics.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
+            graphics.Indices = _indexBuffer;
+            graphics.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 1);
         }
 
         public void Dispose()
